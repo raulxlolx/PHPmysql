030dcfe7b54ba48772e388298bbe706cd1777f11
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-11-29 10:12:01 +0100

    error reporting

--- index.php
+++ index.php
@@ -8,6 +8,8 @@
 </head>
 <body>
     <?php 
+     error_reporting(E_ALL);
+     ini_set('display_errors', 1);
         // Aqui hacemos la conexion a la base de datos
         $server="localhost";
         $user="root";
