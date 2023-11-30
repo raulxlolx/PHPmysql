0856fb2bc0db32be2edddc210b92c3fc2bcd650f
raulxlolx <91206628+raulxlolx@users.noreply.github.com>
2023-11-30 19:09:47 +0100

    arreglo de los discos

--- script.js
+++ script.js
@@ -3,32 +3,28 @@ const disco1 = document.getElementById('disco1');
 const disco2 = document.getElementById('disco2');
 const disco3 = document.getElementById('disco3');
 
+// Crear instancias de Audio una sola vez
+const audio1 = new Audio('recursos/audio/sonido1.mp3');
+const audio2 = new Audio('recursos/audio/sonido2.mp3');
+const audio3 = new Audio('recursos/audio/sonido3.mp3');
+
 // Función para reproducir el sonido
 function reproducirSonido(audio) {
     if (audio.paused) {
         audio.play();
-        
-    } else if (audio.play()) {
-        
+    } else {
         audio.pause();
-        audio.currentTime = 0; // Reiniciar el audio al inicio
+        audio.currentTime = 0;
     }
-
-    
 }
-
-
 
 // Agregar nuevos eventos de clic al disco
 disco1.addEventListener('click', function() {
-    const audio = new Audio('recursos/audio/sonido1.mp3');
-    reproducirSonido(audio);
+    reproducirSonido(audio1);
 });
 disco2.addEventListener('click', function() {
-    const audio = new Audio('recursos/audio/sonido2.mp3');
-    reproducirSonido(audio);
+    reproducirSonido(audio2);
 });
 disco3.addEventListener('click', function() {
-    const audio = new Audio('recursos/audio/sonido3.mp3');
-    reproducirSonido(audio);
+    reproducirSonido(audio3);
 });
