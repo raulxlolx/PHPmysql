5953693a5094d4704738b4b2417949321656f8ee
K1NWI <130763672+K1NWI@users.noreply.github.com>
2023-12-01 13:40:06 +0100

    a

--- bajas.php
+++ bajas.php
@@ -34,8 +34,8 @@
             <a href="index.php"><li>Inicio</li></a>
             <li>Acciones
                 <ul>
-                    <li><a href="">Altas</a></li>
-                    <li><a href="bajas.php">Bajas</a></li>
+                    <li><a href="Alta,php">Altas</a></li>
+                    <li><a href="">Bajas</a></li>
                     <li><a href="#">Modificar</a></li>
                 </ul>
             </li>
@@ -96,6 +96,44 @@
                 echo "<h1>Registro insertado correctamente</h1>";
             } else {
                 echo "Error: " . $sql . "<br>" . $conexion->error;
+            }
+        }
+        ?>
+
+        <h1>Buscar por Título de Álbum</h1>
+        <form action="" method="GET">
+            <label for="titulo_album">Título de Álbum</label>
+            <input type="text" name="titulo_album" id="titulo_album" required>
+            <input type="submit" value="Buscar">
+        </form>
+
+        <?php
+        if (isset($_GET['titulo_album'])) {
+            $titulo_album = $_GET['titulo_album'];
+            $sql = "SELECT * FROM $tabladvds WHERE NombreDVD LIKE '%$titulo_album%'";
+            $result = $conexion->query($sql);
+            if ($result->num_rows > 0) {
+                echo "<table>";
+                echo "<tr><th>ID</th><th>Nombre</th><th>Artista</th><th>Estilo Musical</th><th>Año</th><th>Número de Canciones</th><th>Títulos de Canciones</th><th>Cantidad</th><th>Precio</th><th>Descuento</th><th>IVA</th><th>Acciones</th></tr>";
+                while ($row = $result->fetch_assoc()) {
+                    echo "<tr>";
+                    echo "<td>" . $row['ID_DVD'] . "</td>";
+                    echo "<td>" . $row['NombreDVD'] . "</td>";
+                    echo "<td>" . $row['ArtistaDVD'] . "</td>";
+                    echo "<td>" . $row['EstiloMusicalDVD'] . "</td>";
+                    echo "<td>" . $row['AñoDVD'] . "</td>";
+                    echo "<td>" . $row['NumeroCancionesDVD'] . "</td>";
+                    echo "<td>" . $row['TitulosCancionesDVD'] . "</td>";
+                    echo "<td>" . $row['CantidadDVD'] . "</td>";
+                    echo "<td>" . $row['PrecioDVD'] . "</td>";
+                    echo "<td>" . $row['DescuentoDVD'] . "</td>";
+                    echo "<td>" . $row['IVADVD'] . "</td>";
+                    echo "<td><a href='eliminar.php?id=" . $row['ID_DVD'] . "'>Eliminar</a></td>";
+                    echo "</tr>";
+                }
+                echo "</table>";
+            } else {
+                echo "No se encontraron resultados.";
             }
         }
         ?>
